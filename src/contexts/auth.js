7edfdfde6d805e7883ff5aa3b3a8d72640a2ed8f
lucasofaria <lucas092007@hotmail.com>
2024-01-05 18:00:48 -0300

    App com cadastro funcionando

---
+++ src/contexts/auth.js
@@ -0,0 +1,40 @@
+import React, { createContext, useState } from "react";
+
+import api from "../services/api";
+import { useNavigation } from '@react-navigation/native'
+
+export const AuthContext = createContext({});
+
+function AuthProvider({ children }){
+  const [user, setUser] = useState(null);
+  const [ loadingAuth, setLoadingAuth ] = useState(false);
+
+  const navigation = useNavigation();
+
+  async function signUp(nome, email, password){
+    setLoadingAuth(true);
+
+    try{
+      const response = await api.post('/users', {
+        name: nome,
+        email: email,
+        password: password,
+      })
+
+      setLoadingAuth(false);
+      navigation.goBack();
+
+    }catch(err){
+      console.log("ERRO AO CADASTRAR", err)
+      setLoadingAuth(false)
+    }
+  }
+  
+  return(
+    <AuthContext.Provider value={{ user, signUp, loadingAuth }}>
+      {children}
+    </AuthContext.Provider>
+  )
+}
+
+export default AuthProvider;
--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,8 +1,22 @@
-import React from 'react';
-import { Platform } from 'react-native';
+import React, { useContext, useState } from 'react';
+import { Platform, ActivityIndicator } from 'react-native';
 import { AreaInput, Background, Container, Input, SubmitButton, SubmitText } from '../SignIn/styles';
 
+import { AuthContext } from '../../contexts/auth';
+
 export default function SignUp(){
+  const { signUp, loadingAuth } = useContext(AuthContext)
+
+  const [ nome, setNome ] = useState('');
+  const [ email, setEmail ] = useState('');
+  const [ password, setPassword ] = useState('');
+
+  function handleSignUp(){
+    if(nome === '' || email === '' || password === '') return;
+
+    signUp(nome, email, password);
+  }
+
   return(
     <Background>
       <Container
@@ -12,23 +26,36 @@ export default function SignUp(){
         <AreaInput>
           <Input 
             placeholder="Seu nome"
+            value={nome}
+            onChangeText={ (text) => setNome(text) }
           />
         </AreaInput>
 
         <AreaInput>
           <Input 
             placeholder="Email"
+            value={email}
+            onChangeText={ (text) => setEmail(text) }
           />
         </AreaInput>
 
         <AreaInput>
           <Input 
             placeholder="Senha"
+            value={password}
+            onChangeText={ (text) => setPassword(text) }
+            secureTextEntry={true}
           />
         </AreaInput>
 
-        <SubmitButton>
-          <SubmitText>Cadastrar</SubmitText>
+        <SubmitButton onPress={handleSignUp}>
+          {
+            loadingAuth ? (
+              <ActivityIndicator size={20} color="#FFF" />
+            ) : (
+              <SubmitText>Cadastrar</SubmitText>
+            )
+          }
         </SubmitButton>
       </Container>
     </Background>

dd8876ebe4bbd38318eb20fd2210770f5545bd4f
lucasofaria <lucas092007@hotmail.com>
2024-01-09 15:01:15 -0300

    Registro de receitas funcionando

--- src/pages/New/index.js
+++ src/pages/New/index.js
@@ -1,15 +1,62 @@
 import React, { useState } from 'react';
 
-import { SafeAreaView, TouchableWithoutFeedback, Keyboard } from 'react-native';
+import { SafeAreaView, TouchableWithoutFeedback, Keyboard, Alert } from 'react-native';
 import { Background, Input, SubmitButton, SubmitText } from './styles';
 
 import Header from '../../components/Header';
 import RegisterTypes from '../../components/RegisterTypes';
 
+import api from '../../services/api';
+import { format } from 'date-fns';
+import { useNavigation } from '@react-navigation/native'
+
 export default function New(){
+  const navigation = useNavigation();
+
   const [labelInput, setLabelInput] = useState('');
   const [valueInput, setValueInput] = useState('');
   const [type, setType] = useState('receita');
+
+  function handleSubmit(){
+    Keyboard.dismiss();
+
+    if(isNaN(parseFloat(valueInput)) || type === null){
+      alert('Preencha os campos corretamente!')
+      return;
+    }
+
+    Alert.alert(
+      'Confirmando registro',
+      `Tipo: ${type} | Valor: R$${parseFloat(valueInput)}`,
+      [
+        {
+          text: 'Cancelar',
+          style: 'cancel'
+        },
+        {
+          text: 'Confirmar',
+          onPress: () => handleAdd()
+        }
+      ]
+    )
+  }
+
+  async function handleAdd(){
+    Keyboard.dismiss();
+
+    await api.post('/receive', {
+      description: labelInput,
+      value: Number(valueInput),
+      type: type,
+      date: format(new Date(), 'dd/MM/yyyy')
+    })
+
+    setLabelInput('');
+    setValueInput('');
+
+    navigation.navigate('Home');
+
+  }
 
   return (
     <TouchableWithoutFeedback onPress={ () => Keyboard.dismiss() }>
@@ -37,7 +84,7 @@ export default function New(){
             sendTypeChanged={(item) => setType(item)}
           />
 
-          <SubmitButton>
+          <SubmitButton onPress={handleSubmit}>
             <SubmitText>Registrar</SubmitText>
           </SubmitButton>
         </SafeAreaView>
